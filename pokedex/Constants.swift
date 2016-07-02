fe5833de71cdd6d48c2db1e748afc5e8e5a46697
Fredrik Berglund <frbe9022@git.dsv.su.se>
2016-07-02 22:44:46 +0200

    we just finished our pokedex app

---
+++ pokedex/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by Fredrik Berglund on 2016-07-02.
+//  Copyright © 2016 Fredrik. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
+
+typealias DownloadComplete = () -> ()
---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,29 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Fredrik Berglund on 2016-07-01.
+//  Copyright © 2016 Fredrik. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
